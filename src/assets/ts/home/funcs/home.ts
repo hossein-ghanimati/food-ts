f168fb8b34d3825a172aa43bdcf8f13ac8dd2182
hossein ghaniamti <hosein.ghanimati1398@gmail.com>
2024-11-12 20:59:49 +0330

    Show Categories Dynamic Icon

--- src/assets/ts/home/funcs/home.ts
+++ src/assets/ts/home/funcs/home.ts
@@ -1,11 +1,8 @@
 import { CategoryType } from "@/assets/types/shared/category.type";
 import { generateCategoryTemplate } from "./utils";
-import { emptyTheElem } from "../../utils/elems";
-
 const insertCategoris = (categories: CategoryType[]) => {
   const categoriesWrapper = document.querySelector('.category-list') as HTMLElement;
-    
-  emptyTheElem(categoriesWrapper)
+  
   categories.forEach(category => 
       categoriesWrapper.insertAdjacentHTML(
         "beforeend",
