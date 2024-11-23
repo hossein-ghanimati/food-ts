2e94776882f15a2fa09cb92a7c2b5511e2428bf1
hossein ghaniamti <hosein.ghanimati1398@gmail.com>
2024-11-24 00:28:49 +0330

    Ready To Deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  base: "./",
   server: {
     port: 3000
   }
